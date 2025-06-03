54190f848cd2e13a9b29f51247840ca46d0ee199
Emanuelle Copatti <emanuellecopattti@hotmail.com>
2025-06-02 21:32:39 -0300

    chore(app): ajustes no layout e na navegação por abas

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// Teste de commit
 // src/App.jsx
 import React, { useState, useEffect } from "react";
 import {
--- src/components/tabNavigation.jsx
+++ src/components/tabNavigation.jsx
@@ -1,3 +1,4 @@
+// Teste de commit
 import React from "react";
 import { Tabs, Tab } from "@mui/material";
 
